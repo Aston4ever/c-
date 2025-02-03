8fd2e7a61b88b07fd0b7833788bee0f1797197de
aston <aston4ever@gmail.com>
2025-02-03 14:29:32 +0300

    finish lesson 15

---
+++ Hello_Console/Program.cs
@@ -0,0 +1,11 @@
+﻿int age = 14; // -2 147 483 648 to 2 147 483 647
+int ageEmpty; // 0 by default
+float PI = 3.1415f; // 7 digit precision
+double pi = 3.1415d; // 15 digit precision
+decimal DIdecimal; // 28 digit precision
+sbyte x = 19; // -128 to 127
+short y = -2; //-32767 to 32767
+bool z = true;
+char c = 'a';
+string str = "Hello World!";
+Console.WriteLine($"age: {age}, ageEmpty: {ageEmpty}, PI: {PI}");
