de56335a30f5003609ad397e5e9991f7e80b288a
aston <aston4ever@gmail.com>
2025-02-04 12:32:17 +0300

    add strings

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -7,8 +7,11 @@ void CoolMethod()
 {
     Console.WriteLine(name);
 }
-
-
+string x = Console.ReadLine();
+int x2 = Console.Read();
+Console.Clear();
+Console.WriteLine(x);
+Console.WriteLine(x2);
 string capsName = name.ToUpper();
 
 
