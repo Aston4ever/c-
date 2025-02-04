89bcf06546927e92adeee104c17c4e3167016106
aston <aston4ever@gmail.com>
2025-02-04 12:48:25 +0300

    add conversion

---
+++ Conversion/Program.cs
@@ -0,0 +1,11 @@
+﻿double myDouble = 3.65;
+int myInt;
+
+myInt = (int)myDouble; //explicite conversion (CAST) Просто убираются символы 
+Console.WriteLine(myInt);
+
+float myFloat = 3.6f; //implicite conversion мегьший тип большему типу
+double myDouble2 = myFloat; //Если нет потери данных то можно присваивать значения напряму
+
+//TYpe conversion
+string myString = myDouble2.ToString(); //Преобразование лбого типа в строку
