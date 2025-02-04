bab1ee265b855fa26371aabce5339a3a0157160e
aston <aston4ever@gmail.com>
2025-02-04 12:57:59 +0300

    add parsing

---
+++ Parse/Program.cs
@@ -0,0 +1,9 @@
+﻿string myString1 = "23";
+string myString2 = "56";
+int num1 = int.Parse(myString1); //если не парсится то Unhandled exeption
+int num2 = int.Parse(myString2);
+
+string result = myString1 + myString2;
+int numresult = num1 + num2;
+Console.WriteLine(numresult);
+Console.ReadLine();
