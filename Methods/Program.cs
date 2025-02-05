7afd29bdf1eb9bef57c7865195e3abe977b983d1
aston <aston4ever@gmail.com>
2025-02-05 13:29:57 +0300

    finish HW methods

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -7,3 +7,17 @@
 }
 WriteSmth("Bubl");
 
+int Add(int x, int y)
+{
+  return x + y;
+}
+int result = Add(2, 5);
+int methodInside = Add(Add(5,6), Add(3,4));//Метод как параметр
+Console.WriteLine($"Enter your friend's name");
+string friendName = Console.ReadLine();
+
+void PrintName(string friendName)
+{
+    Console.WriteLine($"Hello from Method!!!{friendName} My Friend!");
+}
+PrintName(friendName);
