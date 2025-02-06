012b060dcc1474c9b7c90e10bba41ee04d4d17fb
aston <aston4ever@gmail.com>
2025-02-06 14:14:50 +0300

    add IF statement

--- IFELSE_switch_ternary/Program.cs
+++ IFELSE_switch_ternary/Program.cs
@@ -7,5 +7,16 @@
  *  } else {
  *      code to execute if FALSE
  *  }
- * 
  */
+ 
+ string temperature = Console.ReadLine();
+ int numTemp;
+
+ if (int.TryParse(temperature, out int temperatureInt))
+ {
+     numTemp = temperatureInt;
+ }
+ else
+ {
+     Console.WriteLine("Please enter a valid temperature");
+ }
