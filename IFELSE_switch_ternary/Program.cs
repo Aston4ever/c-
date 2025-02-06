03d25b2e1d0ed24997d9e0f51632013106c1d77a
aston <aston4ever@gmail.com>
2025-02-06 14:55:35 +0300

    add switch statement

--- IFELSE_switch_ternary/Program.cs
+++ IFELSE_switch_ternary/Program.cs
@@ -1,4 +1,19 @@
-﻿
+﻿int age = 17;
+switch (age)
+{
+    case 1 when age < 18:
+        Console.WriteLine("Hello World!");
+        break;
+    case 2:
+        Console.WriteLine("Hello AGAIN!");
+        break;
+    case 25:
+        Console.WriteLine("You can drive");
+        break;
+    default:
+        Console.WriteLine("no age was found");
+    
+}
 
 
 /*
@@ -7,9 +22,18 @@
  *  } else {
  *      code to execute if FALSE
  *  }
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * 
  */
  
- string temperature = Console.ReadLine();
+ /*
+  string temperature = Console.ReadLine();
  int numTemp;
 
  if (int.TryParse(temperature, out int temperatureInt))
@@ -20,3 +44,40 @@
  {
      Console.WriteLine("Please enter a valid temperature");
  }
+ */
+Console.Write("Enter your UserName to register ");
+string registerUserName = Console.ReadLine();
+Console.Write("Enter your passowrd to register ");
+string registerUserPassword = Console.ReadLine();
+Console.Write("Register Successful");
+Console.Write($"Welcome! Enter your username: ");
+string userNameInput = Console.ReadLine();
+string userPasswordInput;
+
+if (CheckIfUserExists(userNameInput))
+{
+    Console.Write($"Enter your password: ");
+    userPasswordInput = Console.ReadLine();
+    if (CheckIfPasswordCorrect(userPasswordInput))
+    {
+        Console.WriteLine($"Welcome {userNameInput}!");
+    }
+    else
+    {
+        Console.WriteLine("Wrong password!");
+    }
+}
+else
+{
+    Console.WriteLine("Invalid username");
+}
+
+bool CheckIfUserExists(string userName)
+{
+    return userName == registerUserName;
+}
+bool CheckIfPasswordCorrect(string password)
+{
+    return registerUserPassword == userPasswordInput;
+}
+
