fed0059ed9d19ea4f0c28c455d2d37ec4f538a0d
aston <aston4ever@gmail.com>
2025-02-20 12:48:12 +0300

    add foreach

--- COLLECTION/Program.cs
+++ COLLECTION/Program.cs
@@ -4,5 +4,19 @@ int[] nums4 = new[] { 1, 2, 3, 5 };
 int[] nums5 = { 1, 2, 3, 5 };
 
 int[] grades = { 1, 2, 3 };
+//grades[1] = int.Parse(Console.ReadLine());
 
-Console.WriteLine(nums4.Length);
+Console.WriteLine(grades.Length);
+
+for (int i = 0; i < nums2.Length; i++)
+{
+    nums2[i] = i+10;
+    Console.WriteLine(nums2[i]);
+}
+
+Console.WriteLine("--------------------------------");
+foreach (var k in nums3)
+{
+    Console.WriteLine(k);
+}
+
