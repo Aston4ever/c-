f44a6b62e67536d3e0205210c45302b2e4d342be
aston <aston4ever@gmail.com>
2025-03-17 14:14:42 +0300

    Add Queues

---
+++ COLLECTION/Queues.cs
@@ -0,0 +1,23 @@
+﻿namespace COLLECTION;
+
+public class Queues
+{
+    Queue<int> numbers = new Queue<int>();
+
+    void Enqueue(int number)
+    {
+        numbers.Enqueue(number);  // Добавляет элемент в конец очереди
+    }
+
+    void Peek()
+    {
+        Console.WriteLine(numbers.Peek()); //Показывает первый элемент в очереди
+    }
+
+    void Dequeue()
+    {
+        int dequedNum = numbers.Dequeue(); //Убирает элемент из очереди. Можно его так же поместить в переменную
+        Console.WriteLine(numbers.Count);
+        
+    }
+}
