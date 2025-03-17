b7e16f89d973c8f86ca4ed4138e6494b2d82ca2e
aston <aston4ever@gmail.com>
2025-03-17 13:49:54 +0300

    add Stack

---
+++ COLLECTION/Stack.cs
@@ -0,0 +1,23 @@
+﻿namespace COLLECTION;
+
+public class Stack
+{
+    Stack<int> stack = new Stack<int>();
+
+    void Push(int value)
+    {
+        stack.Push(value);
+        stack.Count();
+    }
+
+    void Peek()
+    {
+        stack.Peek(); // показывает самый верхний элемент
+    }
+
+    void Pop()
+    {
+        int myPoppedItem = stack.Pop(); // Удаляет элемент сверху. Если присвоить - то так же записывает удаленное значение в переменную
+        
+    }
+}
